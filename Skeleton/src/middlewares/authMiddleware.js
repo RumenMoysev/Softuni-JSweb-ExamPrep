f1d0375910d3c53ab3972de51db2a8b0c3ada1d8
RumenMoysev <rumoysev@gmail.com>
2023-10-13 21:50:30 +0300

    Add reactive navBar

---
+++ Skeleton/src/middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('../lib/jwtPromise.js')
+const SECRET = require('../configs/config.js')
+
+exports.isAuth = async (req, res, next)  => {
+    const token = req.cookies['auth']
+    console.log(token)
+
+    if(token) {
+
+        try {
+            const user = await jwt.verify(token, SECRET)
+            req.user = user
+            res.locals.isAuthenticated = true
+            next()
+        } catch(err) {
+            res.clearCookie('auth')
+            res.locals.isAuthenticated = false
+
+            res.redirect('/')
+        }
+    } else {
+        res.locals.isAuthenticated = false
+        next()
+    }
+}
