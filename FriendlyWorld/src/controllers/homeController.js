ab79871c083e93a1ee05fb8e5b82efbfaf221b86
RumenMoysev <rumoysev@gmail.com>
2023-10-18 00:01:36 +0300

    Fixed home page

--- FriendlyWorld/src/controllers/homeController.js
+++ FriendlyWorld/src/controllers/homeController.js
@@ -1,7 +1,19 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.render('home')
+const animalManager = require('../managers/animalManager.js')
+
+router.get('/', async (req, res) => {
+    let animalArr = []
+    const animals = await animalManager.getAnimalsLean()
+
+    if(animals.length > 3) {
+        for (let i = 0; i < 3; i++) {
+            animalArr.push(animals[i])
+        }
+    }
+    animalArr = animals
+
+    res.render('home', {animalArr})
 })
 
 router.get('/404', (req, res) => {
