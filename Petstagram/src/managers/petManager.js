37c0fd5be433339b568ef33aefac87651b65753f
RumenMoysev <rumoysev@gmail.com>
2023-10-18 22:51:22 +0300

    Formatted petManager and details page

--- Petstagram/src/managers/petManager.js
+++ Petstagram/src/managers/petManager.js
@@ -6,22 +6,22 @@ const descriptionLength = [5, 50]
 const locationLength = [5, 50]
 
 function validate(petData) {
-    if(isNaN(petData.age)) {
+    if (isNaN(petData.age)) {
         throw new Error('Age should be a number')
     }
-    if(petData.name.length < nameLength) {
+    if (petData.name.length < nameLength) {
         throw new Error(`Name should be at least ${nameLength} characters long.`)
     }
-    if(!petData.image.startsWith('http://') && !petData.image.startsWith('https://')) {
+    if (!petData.image.startsWith('http://') && !petData.image.startsWith('https://')) {
         throw new Error('Please provide a valid image URL')
     }
-    if(Number(petData.age) < age[0] || Number(petData.age) > age[1]) {
+    if (Number(petData.age) < age[0] || Number(petData.age) > age[1]) {
         throw new Error(`Age should be between ${age[0]} and ${age[1]}`)
     }
-    if(petData.description.length < descriptionLength[0] || petData.description.length > descriptionLength[1]) {
+    if (petData.description.length < descriptionLength[0] || petData.description.length > descriptionLength[1]) {
         throw new Error(`Description should be between ${descriptionLength[0]} and ${descriptionLength[1]} characters long.`)
     }
-    if(petData.location.length < locationLength[0] || petData.location.length > locationLength[1]) {
+    if (petData.location.length < locationLength[0] || petData.location.length > locationLength[1]) {
         throw new Error(`Location should be between ${locationLength[0]} and ${locationLength[1]} characters long.`)
     }
 }
@@ -55,7 +55,7 @@ exports.deletePet = (id) => Pet.findByIdAndDelete(id)
 
 exports.addCommentAndGetData = async (userId, comment, petId) => {
     try {
-        if(comment.length < 1) {
+        if (comment.length < 1) {
             throw new Error('Comment should be at least 1 character long')
         }
 
@@ -72,5 +72,5 @@ exports.addCommentAndGetData = async (userId, comment, petId) => {
         return Pet.findById(petId).populate('owner').populate('commentList').lean()
     } catch (error) {
         throw new Error(error.message)
-    } 
+    }
 }
