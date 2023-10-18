4949bfafc04f724e99da9cf319eae17ff4b15067
RumenMoysev <rumoysev@gmail.com>
2023-10-18 11:56:59 +0300

    Add catalog page

--- Petstagram/src/routes.js
+++ Petstagram/src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 
 const homeController = require('./controllers/homeController.js')
 const usersController = require('./controllers/userController.js')
+const petController = require('./controllers/petsController.js')
 
 router.use(homeController)
 router.use('/users', usersController)
+router.use('/pets', petController)
 router.use('*', (req, res) => {
     res.redirect('404')
 })
