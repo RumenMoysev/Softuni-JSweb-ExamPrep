5777995a509d8569f852534014902130ad2b3252
RumenMoysev <rumoysev@gmail.com>
2023-10-21 14:34:28 +0300

    Add Electronic model

--- SecondHandElectronics/src/controllers/electronicsController.js
+++ SecondHandElectronics/src/controllers/electronicsController.js
@@ -1,7 +1,15 @@
 const router = require('express').Router()
 
+const electronicsManager = require('../managers/electronicsManager.js')
+
 router.get('/catalog', async (req, res) => {
-    res.render('electronicTemps/catalog')
+    try {
+        const electronics = await electronicsManager.getElectronicsLean()
+        res.render('electronicTemps/catalog', {electronics})
+    } catch (error) {
+        console.log(error.message)
+        res.render('electronicTemps/catalog')
+    }
 })
 
 module.exports = router
